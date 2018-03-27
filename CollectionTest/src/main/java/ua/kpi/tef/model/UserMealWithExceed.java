0889163e60e7050a22588197c2f2c8abc8ee340f
Vitalii Hrushyn <vhrushyn@gmail.com>
2018-03-27 12:21:12 +0300

    Collection test commit - cycle approach

---
+++ CollectionTest/src/main/java/ua/kpi/tef/model/UserMealWithExceed.java
@@ -0,0 +1,41 @@
+package ua.kpi.tef.model;
+
+import java.time.LocalDateTime;
+
+/**
+ * GKislin
+ * 11.01.2015.
+ */
+public class UserMealWithExceed {
+    private final LocalDateTime dateTime;
+
+    private final String description;
+
+    private final int calories;
+
+    private final boolean exceed;
+
+    public UserMealWithExceed(LocalDateTime dateTime, String description, int calories, boolean exceed) {
+        this.dateTime = dateTime;
+        this.description = description;
+        this.calories = calories;
+        this.exceed = exceed;
+    }
+
+    public UserMealWithExceed(UserMeal userMeal, boolean exceed) {
+        this.dateTime = userMeal.getDateTime();
+        this.description = userMeal.getDescription();
+        this.calories = userMeal.getCalories();
+        this.exceed = exceed;
+    }
+
+    @Override
+    public String toString() {
+        return "\nUserMealWithExceed: " +
+                "date and time = " + dateTime +
+                ", description = '" + description + '\'' +
+                ", calories = " + calories +
+                ", exceed = " + exceed +
+                ".";
+    }
+}
